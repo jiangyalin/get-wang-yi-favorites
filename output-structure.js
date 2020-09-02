3d316a6f94dacc29741211e63397ed557ff3737a
yalin.jiang@asapone.cn <995991>
2020-09-02 10:25:37 +0800

    查找文件由匹配改为寻找相似度最高的文件

--- output-structure.js
+++ output-structure.js
@@ -1,37 +1,28 @@
 // 按收藏分类输出（第二步）
 const fs = require('fs')
+const stringSimilarity = require('string-similarity')
 const enter = require('./config').enter
 const readData = require('./tool/read-data')
 const createFolder = require('./tool/create-folder')
 const ncm = require('./tool/ncm')
 
+const readDir = fs.readdirSync(enter)
+
 const list = readData('favorites')
-
-const suffix = ['.mp3', '.flac', '.ncm']
-
-const _list = []
 
 list.forEach(item => {
   const path = './output/' + item.name
   createFolder(path)
   item.node.forEach(data => {
-    suffix.forEach(node => {
-      const fileName = data.author.replace(/\//g, ',').replace(/ /g, ' ') + ' - ' + data.name.replace(/ /g, ' ')
-        .replace(/\?/g, '？').replace(/:/g, '：')
-      if (fs.existsSync(enter + '/' + fileName + node)) {
-        if (node === '.ncm') {
-          ncm(enter + '/' + fileName + node, path + '/' + fileName + '.flac', fileName)
-        } else {
-          fs.copyFileSync(enter + '/' + fileName + node, path + '/' + fileName + node)
-        }
-      } else {
-        _list.push(fileName)
-      }
-    })
+    const fileName = data.author.replace(/\//g, ',').replace(/ /g, ' ') + ' - ' + data.name.replace(/ /g, ' ')
+      .replace(/\?/g, '？').replace(/：/g, ':').replace(/é/g, 'é')
+    const matches = stringSimilarity.findBestMatch(fileName, readDir)
+    const thisSuffix = matches.bestMatch.target.substring(matches.bestMatch.target.lastIndexOf('.'))
+    if (thisSuffix === '.ncm') {
+      ncm(enter + '/' + matches.bestMatch.target, path + '/' + fileName + '.flac', fileName)
+    } else {
+      fs.copyFileSync(enter + '/' + matches.bestMatch.target, path + '/' + matches.bestMatch.target)
+    }
   })
 })
 
-// "name":"Forever...","author":"savage genius","title":"Forever... - ( )"
-
-console.log('_list', _list.filter(item => _list.filter(node => node === item).length === 3))
-console.log('aaa', _list.filter(item => _list.filter(node => node === item).length === 3).length / 3)
