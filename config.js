127752b24a2331a877dae5ddc855c0224840258f
jiangyalin <jiangyl>
2024-06-11 14:28:47 +0800

    feat: 配置项

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
+require('dotenv').config()
+
+const env = process.env
+
 module.exports = {
   port: 8085,
   url: 'https://music.163.com/#/my/m/music/playlist',
-  id: '',
-  account: '',
-  password: '',
+  id: env.ID,
+  // 网易云音乐账号
+  account: env.ACCOUNT,
+  // 网易云音乐密码
+  password: env.PASSWORD,
   enter: '/Users/jiangyalin/Music/网易云音乐'
 }
